fbef2d88e4b7dd9e5ce33285776990d3530f87e7
rizaadi <rizaadi890@gmail.com>
2024-03-14 15:25:12 +0700

    fix: add header contenttype agent

--- lib/features/agent/data/datasources/remote/remote_datasource.dart
+++ lib/features/agent/data/datasources/remote/remote_datasource.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
 
 import 'package:valorantlab/core/constants/strings_constant.dart';
 import 'package:valorantlab/core/error/failure.dart';
 import 'package:valorantlab/features/agent/data/datasources/remote/models/agent.dart';
-import 'package:http/http.dart' as http;
 
 abstract class RemoteDataSource {
   Future<List<AgentModel>> getAgents();
@@ -20,7 +22,12 @@ class RemoteDatasourceImpl implements RemoteDataSource {
       // to make sure don't have a "duplicate" Sova
       'isPlayableCharacter': 'true',
     });
-    final response = await client.get(url);
+    final response = await client.get(
+      url,
+      headers: {
+        HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8',
+      },
+    );
 
     if (response.statusCode == 200 || response.statusCode == 304) {
       final List<dynamic> data = jsonDecode(response.body)['data'];
@@ -33,7 +40,12 @@ class RemoteDatasourceImpl implements RemoteDataSource {
   @override
   Future<AgentModel> getAgentById(String agentId) async {
     final url = Uri.parse('$baseUrl/agents/$agentId');
-    final response = await client.get(url);
+    final response = await client.get(
+      url,
+      headers: {
+        HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8',
+      },
+    );
 
     if (response.statusCode == 200 || response.statusCode == 304) {
       return AgentModel.fromJson(jsonDecode(response.body)['data']);
