95a9c2d876684aa373a7c566e601290c5f0b2ea8
rizaadi <rizaadi890@gmail.com>
2024-03-21 11:09:20 +0700

    test: add connection checker test

---
+++ test/core/network/connection_checker_test.dart
@@ -0,0 +1,55 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:valorantlab/core/network/connection_checker.dart';
+
+class MockConnectivity extends Mock implements Connectivity {}
+
+void main() {
+  late ConnectionCheckerImpl connectionChecker;
+  late MockConnectivity mockConnectivity;
+
+  setUp(() {
+    mockConnectivity = MockConnectivity();
+    connectionChecker = ConnectionCheckerImpl(connectivity: mockConnectivity);
+  });
+
+  group('isConnected', () {
+    test('should return ConnectivityResult.wifi', () async {
+      final tConnectionResultNoneFuture = Future.value(ConnectivityResult.wifi);
+
+      when(() => mockConnectivity.checkConnectivity())
+          .thenAnswer((_) => tConnectionResultNoneFuture);
+
+      final result = connectionChecker.connectivity.checkConnectivity();
+      verify(() => mockConnectivity.checkConnectivity());
+
+      expect(result, tConnectionResultNoneFuture);
+    });
+
+    test('should return ConnectivityResult.mobile', () {
+      final tConnectionResultNoneFuture =
+          Future.value(ConnectivityResult.mobile);
+
+      when(() => mockConnectivity.checkConnectivity())
+          .thenAnswer((_) => tConnectionResultNoneFuture);
+
+      final result = connectionChecker.connectivity.checkConnectivity();
+      verify(() => mockConnectivity.checkConnectivity());
+
+      expect(result, tConnectionResultNoneFuture);
+    });
+
+    test('should return true when internet connection is connected', () async {
+      const tConnectionResultFuture = true;
+
+      when(() => mockConnectivity.checkConnectivity())
+          .thenAnswer((invocation) => Future.value(ConnectivityResult.wifi));
+
+      final result = await connectionChecker.isConnected;
+      verify(() => mockConnectivity.checkConnectivity());
+
+      expect(result, tConnectionResultFuture);
+    });
+  });
+}
