c46f2cad1cfe96f5f2c205e8d117b1fa34eabaa5
rizaadi <rizaadi890@gmail.com>
2024-03-26 13:56:07 +0700

    test: add agent repository test

--- test/features/agent/data/repositories/agent_repository_test.dart
+++ test/features/agent/data/repositories/agent_repository_test.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:valorantlab/core/error/failure.dart';
 import 'package:valorantlab/core/network/connection_checker.dart';
 import 'package:valorantlab/features/agent/data/datasources/local/agent_local_data_source.dart';
 import 'package:valorantlab/features/agent/data/datasources/mappers/remote_mapper.dart';
@@ -34,65 +36,191 @@ void main() {
     );
   });
 
-  // void runTestsOnline(Function body) {
-  //   group('device is online', () {
-  //     setUp(() {
-  //       when(() => mockConnectionChecker.isConnected)
-  //           .thenAnswer((invocation) async => true);
-  //     });
+  void runTestsOnline(Function body) {
+    group('device is online', () {
+      setUp(() {
+        when(() => mockConnectionChecker.isConnected)
+            .thenAnswer((invocation) async => true);
+      });
 
-  //     body();
-  //   });
-  // }
+      body();
+    });
+  }
 
-  // void runTestsOffline(Function body) {
-  //   group('device is offline', () {
-  //     setUp(() {
-  //       when(() => mockConnectionChecker.isConnected)
-  //           .thenAnswer((invocation) async => true);
-  //     });
+  void runTestsOffline(Function body) {
+    group('device is offline', () {
+      setUp(() {
+        when(() => mockConnectionChecker.isConnected)
+            .thenAnswer((invocation) async => false);
+      });
 
-  //     body();
-  //   });
-  // }
+      body();
+    });
+  }
 
   group('getAgents', () {
     final jsonAgent = fixture('agent/agents.json');
     final List<dynamic> decodedAgent = jsonDecode(jsonAgent)['data'];
     final apiAgents = decodedAgent.map((e) => ApiAgent.fromJson(e)).toList();
     final localAgents = apiAgents.map((e) => e.toLocal()).toList();
-    // final entityAgent = apiAgents.map((e) => e.toEntity()).toList();
 
     setUp(() {
-      when(() => mockLocalDataSource.putAgents(localAgents))
-          .thenAnswer((_) async {});
+      when(() => mockLocalDataSource.putAgents(any())).thenAnswer((_) async {});
       when(() => mockRemoteDataSource.getAgents())
           .thenAnswer((_) async => apiAgents);
     });
 
     test('should check if the device is online', () {
+      when(() => mockRemoteDataSource.getAgents())
+          .thenAnswer((_) async => apiAgents);
       when(() => mockConnectionChecker.isConnected)
           .thenAnswer((_) async => true);
+
       agentRepository.getAgents();
+
       verify(() => mockConnectionChecker.isConnected);
     });
 
-    // runTestsOnline(() {
-    //   test(
-    //       'should return remote data when the call to remote data source is successfull',
-    //       () async {
-    //     when(() => mockRemoteDataSource.getAgents())
-    //         .thenAnswer((_) async => apiAgents);
-    //     when(() => mockLocalDataSource.putAgents(localAgents))
-    //         .thenAnswer((_) async {});
+    runTestsOnline(() {
+      test(
+          'should return remote data when the call to remote data source is successfull',
+          () async {
+        when(() => mockRemoteDataSource.getAgents())
+            .thenAnswer((_) async => apiAgents);
 
-    //     final result = await agentRepository.getAgents();
+        final result = await agentRepository.getAgents();
 
-    //     verify(() async => await mockRemoteDataSource.getAgents());
-    //     verify(() => mockLocalDataSource.putAgents(localAgents));
+        verify(() => mockRemoteDataSource.getAgents());
+        expect(result, isA<Right>());
+      });
 
-    //     expect(result, equals(Right(entityAgent)));
-    //   });
-    // });
+      test(
+          'should cache the data locally when the call to remote data source is successfull',
+          () async {
+        when(() => mockRemoteDataSource.getAgents())
+            .thenAnswer((_) async => apiAgents);
+
+        final result = await agentRepository.getAgents();
+
+        verify(() => mockRemoteDataSource.getAgents());
+        verify(() => mockLocalDataSource.putAgents(any()));
+        expect(result, isA<Right>());
+      });
+
+      test(
+          'should return server failure when the call to remote data source is unsuccessful',
+          () async {
+        when(() => mockRemoteDataSource.getAgents()).thenThrow(ServerFailure());
+
+        final result = await agentRepository.getAgents();
+
+        verify(() => mockRemoteDataSource.getAgents());
+        verifyZeroInteractions(mockLocalDataSource);
+        expect(result, equals(Left(ServerFailure())));
+      });
+    });
+
+    runTestsOffline(() {
+      test('should return last local data when the cached data is present',
+          () async {
+        when(() => mockLocalDataSource.getAgents())
+            .thenAnswer((_) async => localAgents);
+
+        final result = await agentRepository.getAgents();
+
+        verifyZeroInteractions(mockRemoteDataSource);
+        verify(() => mockLocalDataSource.getAgents());
+        expect(result, isA<Right>());
+      });
+
+      test(
+          'should return cache failure when the call to local data source is unsuccessful',
+          () async {
+        when(() => mockLocalDataSource.getAgents()).thenThrow(CacheFailure());
+
+        final result = await agentRepository.getAgents();
+
+        verifyZeroInteractions(mockRemoteDataSource);
+        verify(() => mockLocalDataSource.getAgents());
+        expect(result, equals(Left(CacheFailure())));
+      });
+    });
+  });
+
+  group('getAgentById', () {
+    const agentId = 'add6443a-41bd-e414-f6ad-e58d267f4e95';
+    final jsonAgent = fixture('agent/agent.json');
+    final decodedAgent = jsonDecode(jsonAgent)['data'];
+    final apiAgent = ApiAgent.fromJson(decodedAgent);
+    final entityAgent = apiAgent.toEntity();
+
+    setUp(() {
+      when(() => mockRemoteDataSource.getAgentById(agentId))
+          .thenAnswer((_) async => apiAgent);
+    });
+
+    test('should check if the device is online', () {
+      when(() => mockRemoteDataSource.getAgentById(agentId))
+          .thenAnswer((_) async => apiAgent);
+      when(() => mockConnectionChecker.isConnected)
+          .thenAnswer((_) async => true);
+
+      agentRepository.getAgentById(agentId);
+
+      verify(() => mockConnectionChecker.isConnected);
+    });
+
+    runTestsOnline(() {
+      test(
+          'should return remote data when the call to remote data source is successfull',
+          () async {
+        when(() => mockRemoteDataSource.getAgentById(agentId))
+            .thenAnswer((_) async => apiAgent);
+
+        final result = await agentRepository.getAgentById(agentId);
+
+        verify(() => mockRemoteDataSource.getAgentById(agentId));
+        expect(result, equals(Right(entityAgent)));
+      });
+
+      test(
+          'should return server failure when the call to remote data source is unsuccessful',
+          () async {
+        when(() => mockRemoteDataSource.getAgentById(agentId))
+            .thenThrow(ServerFailure());
+
+        final result = await agentRepository.getAgentById(agentId);
+
+        verify(() => mockRemoteDataSource.getAgentById(agentId));
+        expect(result, equals(Left(ServerFailure())));
+      });
+    });
+
+    runTestsOffline(() {
+      test('should return last local data when the cached data is present',
+          () async {
+        when(() => mockLocalDataSource.getAgentById(agentId))
+            .thenAnswer((_) async => apiAgent.toLocal());
+
+        final result = await agentRepository.getAgentById(agentId);
+
+        verifyZeroInteractions(mockRemoteDataSource);
+        verify(() => mockLocalDataSource.getAgentById(agentId));
+        expect(result, equals(Right(entityAgent)));
+      });
+
+      test(
+          'should return cache failure when the call to local data source is unsuccessful',
+          () async {
+        when(() => mockLocalDataSource.getAgentById(agentId))
+            .thenThrow(CacheFailure());
+
+        final result = await agentRepository.getAgentById(agentId);
+
+        verifyZeroInteractions(mockRemoteDataSource);
+        verify(() => mockLocalDataSource.getAgentById(agentId));
+        expect(result, equals(Left(CacheFailure())));
+      });
+    });
   });
 }
